463e6a1598d92621e8e354fc3be9489255dfcb09
Filipe <piracicabafilipe@gmail.comdir>
2026-05-04 19:48:52 -0300

    12th update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vehicles brands analysis app
+
+This app is a useful tool for quick analyis. You can customize charts according to your interest and compare different types of vehicles.
---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.header('vehicles analysis')
+
+df = pd.read_csv("notebooks/vehicles.csv")
+
+hist_checkbox = st.checkbox('Create histogram')
+
+if hist_checkbox: # with button click
+    st.write('Creating an histogram to the odometer column')
+
+    fig = px.histogram(df, x="odometer")
+
+    st.plotly_chart(fig, use_container_width = True)
+
+plotly_express = st.checkbox('Create a scatter chart price vs odometer')
+
+if plotly_express: # if clicked
+    st.write('Creating a scatter chart')
+
+    fig_01 = px.scatter(df, x='odometer', y='price')
+
+    st.plotly_chart(fig_01)
+
